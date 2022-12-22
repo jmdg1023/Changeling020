2721fc2112b2a1e0d2f44621fe6ec5c8ec73d3de
Astrid <jmdg1023@gmail.com>
2022-12-22 13:01:00 +1100

    debugging

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
